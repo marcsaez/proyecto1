edaf6e6e02ed5664198fe474aea94146a01baefe
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2023-09-20 12:23:36 +0200

    cosas

--- FRONTEND/menuadmin.php
+++ FRONTEND/menuadmin.php
@@ -8,8 +8,10 @@
 <body>
 
     <a href="./formulariocursos.php"><h2>Crear cursos</h2></a>
-    <a href="./modificarcurso.php"><h2>Modificar cursos</h2>
+    <a href="./formulariomodificarcursos.php"><h2>Modificar cursos</h2>
     <a href="./eliminarcurso.php"><h2>Eliminar cursos</h2>
-
+    <a href="./formularioprofes.php"><h2>Crear Profesores</h2>
+    <a href="./eliminarcurso.php"><h2>Modifiar Profesores</h2>
+    <a href="./profesdesactivar.php"><h2>Desactivar Profesores</h2>
 </body>
 </html>
