d72df7de1b72273409081cd6eabe3ae616d7c1e3
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2023-09-20 13:44:24 +0200

    DESACTIVAR HECHO

--- FRONTEND/desactivarprofe.php
+++ FRONTEND/desactivarprofe.php
@@ -1,5 +1,52 @@
 <?php 
 include_once("funciones.php");
 
+    if($_POST){
+        
+        if (isset($_POST['profesores']) && is_array($_POST['profesores'])) {
+            $profesoresSeleccionados = $_POST['profesores'];
+    
+            // Realizar la conexión a la base de datos 
+            $connection = abrirBBDD();
+    
+            if ($connection) {
+                foreach ($profesoresSeleccionados as $dni){
+                    $busqueda = "SELECT activo FROM profesor WHERE dni = ?"; 
+                    $stmt_busqueda = $connection->prepare($busqueda);
+                        if ($stmt_busqueda) {
+                            $stmt_busqueda->bind_param("s", $dni);
+                            $stmt_busqueda->execute();
+                            $stmt_busqueda->bind_result($activo);
+                    
+                            if ($stmt_busqueda->fetch()) {
+                                // Si el resultado indica que está activo, desactívalo, y viceversa
+                                $stmt_busqueda->close();
+                                if ($activo == 1) {
+                                    $sql = "UPDATE profesor SET activo = 0 WHERE dni = ?";
+                                } else {
+                                    $sql = "UPDATE profesor SET activo = 1 WHERE dni = ?";
+                                }
+                    
+                                $stmt = $connection->prepare($sql);
+                                if ($stmt) {
+                                    $stmt->bind_param("s", $dni);
+                                    $stmt->execute();
+                                    $stmt->close();
+                                }
+                            }
+                        }
+                    }
+                    
+                    header("Location: profesdesactivar.php");
+                }
+        } else{
+            header("Location: profesdesactivar.php");
+
+        }
+    } else{
+        echo "<h1>No se relleno el formulario correctamente</h1>";
+        sleep(1);
+        header("Location: menuadmin.php");
+}
 
 ?>
--- FRONTEND/profesdesactivar.php
+++ FRONTEND/profesdesactivar.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desactivar Profes</title>
+    <title>Gestionar Profes</title>
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
 </head>
 <body>
+    
 <header>
         <div class="header">
             <img src="./img/TECHrecortada.png" alt="TechAcademy" id="logo">
@@ -21,6 +22,55 @@
             <img src="./img/98-1.jpg" alt="fotoperfil" id="fotoperfil">
         </div>
     </header>
+    <h2>Desactivar Profesor:</h2>
+
+    <form method="POST" action="desactivarprofe.php">
+        <?php 
+        include_once("funciones.php");
+        $connection = abrirBBDD();
+        if ($connection) {
+            $sql = "SELECT * FROM profesor WHERE activo = 1";
+            $result = $connection->query($sql);
+
+            if ($result) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $dni = $row['dni'];
+                    $nombre = $row['nombre'];
+
+                    echo "<label for='profesor_$dni'><input type='checkbox' name='profesores[]' id='profesor_$dni' value='$dni'> $nombre ($dni)</label><br>";
+                }
+            }
+            
+            // Cierra la conexión a la base de datos
+            $connection = null;
+        }
+        ?>
+        <input type="submit" value="Desactivar">
+    </form>
+        <h2>Activar Profesor:</h2>
+    <form method="POST" action="desactivarprofe.php">
+        <?php 
+        include_once("funciones.php");
+        $connection = abrirBBDD();
+        if ($connection) {
+            $sql = "SELECT * FROM profesor WHERE activo = 0";
+            $result = $connection->query($sql);
+
+            if ($result) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $dni = $row['dni'];
+                    $nombre = $row['nombre'];
+
+                    echo "<label for='profesor_$dni'><input type='checkbox' name='profesores[]' id='profesor_$dni' value='$dni'> $nombre ($dni)</label><br>";
+                }
+            }
+            
+            // Cierra la conexión a la base de datos
+            $connection = null;
+        }
+        ?>
+        <input type="submit" value="Activar">
+    </form>
 
     <footer>
         <div class="contacto">
