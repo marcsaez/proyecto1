e6deb457111a71e0bf132017f540c4ecaca57989
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2023-09-21 08:59:22 +0200

    FINAL

--- FRONTEND/menuadmin.php
+++ FRONTEND/menuadmin.php
@@ -4,14 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
 </head>
 <body>
+<header>
+        <div class="header">
+            <img src="./img/TECHrecortada.png" alt="TechAcademy" id="logo">
+            <h2 id="titulo">TECH ACADEMY</h2>
+        </div>
+        <div class="usuario">
+            <!-- FOTO DE PRUEBA PARA MEDIDAS (HAY QUE SACARLA DE LA BBDD) -->
+            <p id="username" >Pablo de Gregorio</p>
+            <img src="./img/98-1.jpg" alt="fotoperfil" id="fotoperfil">
+        </div>
+    </header>
 
     <a href="./formulariocursos.php"><h2>Crear cursos</h2></a>
-    <a href="./formulariomodificarcursos.php"><h2>Modificar cursos</h2>
-    <a href="./eliminarcurso.php"><h2>Eliminar cursos</h2>
-    <a href="./formularioprofes.php"><h2>Crear Profesores</h2>
-    <a href="./formulariomodificarprofes.php"><h2>Modificar Profesores</h2>
-    <a href="./profesdesactivar.php"><h2>Desactivar Profesores</h2>
+    <a href="./formulariomodificarcursos.php"><h2>Modificar cursos</h2></a>
+    <a href="./eliminarcurso.php"><h2>Eliminar cursos</h2></a>
+    <a href="./formularioprofes.php"><h2>Crear Profesores</h2></a>
+    <a href="./formulariomodificarprofes.php"><h2>Modificar Profesores</h2></a>
+    <a href="./profesdesactivar.php"><h2>Desactivar Profesores</h2></a>
+
+    <footer>
+        <div class="contacto">
+            <p>consultas@techacademy.com</p>
+            <p>C/de la Batlloria, Badalona</p>
+        </div>
+        <div class="copyright">
+            <p>© 2023 TECH ACADEMY</p>
+        </div>
+    </footer>
 </body>
 </html>
