1ca2be7dab97f0eb86fd1d00fc74bc440a39b81a
marc.saez <marc.saez@inslapineda.cat>
2023-09-27 12:31:45 +0200

    Pagina Curso Individual

---
+++ FRONTEND/paginacurso.php
@@ -0,0 +1,46 @@
+<?php 
+    session_start();
+    $dni = $_SESSION['dni'];
+    include_once('funciones.php');
+    $datos = sessionAbrir($dni);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado cursos</title>
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <script src = "./js/concurso.js"></script>
+    <header>
+        <div class="header">
+        <a href="menuadmin.php"><img src="./img/TECHrecortada.png" alt="TechAcademy" id="logo"></a>
+            <h2 id="titulo">TECH ACADEMY</h2>
+        </div>
+        <?php 
+            datosUserVisibles($datos);
+        ?>
+    </header>
+    <?php
+        if (isset($_POST['ver_curso'])) {
+            $codigo = $_POST['codigo'];
+            mostrarCurso($codigo);
+        }
+    ?>
+    <footer>
+        <div class="contacto">
+            <p>consultas@techacademy.com</p>
+            <p>C/de la Batlloria, Badalona</p>
+        </div>
+        <div class="copyright">
+            <p>© 2023 TECH ACADEMY</p>
+        </div>
+    </footer>
+
+</body>
+</html>
