b737b487670455b205e35d85466d9b95f96e6c7b
PaablooDR_ <pab25702@gmail.com>
2023-09-28 10:44:47 +0200

    scss

--- FRONTEND/login.php
+++ FRONTEND/login.php
@@ -8,6 +8,9 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
     <link rel="stylesheet" type="text/css" href="./css/inicio.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php
