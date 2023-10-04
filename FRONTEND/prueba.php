afba79794773414d024fbad49258f14cdd068e1d
marc.saez <marc.saez@inslapineda.cat>
2023-10-04 14:16:37 +0200

    Funcion encabezado y scss modificado

---
+++ FRONTEND/prueba.php
@@ -0,0 +1,24 @@
+<?php 
+    // CREADO MOMENTARIAMENTE PARA HACER PRUEBAS CON EL HEADER
+    session_start();
+    $dni = $_SESSION['dni'];
+    include_once('funciones.php');
+    include_once('header.php');
+    $datos = sessionAbrir($dni);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado cursos</title>
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php
+        encabezadoUsuario($datos)
+?>
+</body>
